0e7e4a30c0acef1f8ed5a17052e2d3172a75062a
farsana nkm <fasrsanankm24@gmail.comgit config --global user.email fasrsanagit config --global user.email fasrsanankm24@gmail.com>
2024-04-18 13:42:12 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,50 +1,59 @@
 import logo from './logo.svg';
 // import './Hai.css';
-import Hello from'./Component/Hello'
+// import Hello from './Component/Hello'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import Card from './Component/Project1';
-import Project1 from './Component/Project1';
+// import Project1 from './Component/Project1';
+import Proj2 from './Component/Proj2';
+import image1 from '../src/Component/images/kalpana.jpeg'
+import image2 from '../src/Component/images/rithu.jpeg'
+import image3 from '../src/Component/images/th.jpeg'
+
 
 function App() {
 
   const data = [
 
     {
-      id:1,
-      name:"ashudhas",
-      img:"https://th.bing.com/th/id/OIP.8eJNMEsDP4uYuV4dl--i9gHaE8?w=356&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+      id: 1,
+      name: "Kalpana Chawla",
+      img : image1,
+      description:"Kalpana Chawla was an indian born American astronautand aerospace engineer who was the first women of india originto fly to space . she first flew on space shuttle columbia in1997 as a mission specialist and primary robotic arm operator aboard STS-87.Kalpana Chawla was an indian born American astronautand aerospace engineer who was the first women of india originto fly to space Kalpana Chawla was an indian born American astronautand aerospace engineer who was the first women of india originto fly to space"
     },
-    
+
     {
-      id:1,
-      name:"ashudhas",
-      img:"https://th.bing.com/th/id/OIP.8eJNMEsDP4uYuV4dl--i9gHaE8?w=356&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+      id: 2,
+      name: "Ritu Karidhal",
+      img : image2,
+      description: "Ritu Karidhal Srivastava is an Indian scientist and aerospace engineer working in the Indian Space Research Organisation(ISRO).She was a Deputy Operations Director to India's Mars orbitalmission, Mangalyaan.She has been referred to as one of the many Rocket Womem of India.Giving tribute to her excellence in Mangalayaan mission, a Bollywood film Mission Mangal was createdwhere Indian actress Vidya Balan did her role."
+
     },
-    
+
     {
-      id:1,
-      name:"ashudhas",
-      img:"https://th.bing.com/th/id/OIP.8eJNMEsDP4uYuV4dl--i9gHaE8?w=356&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
-    },
-    
-    {
-      id:1,
-      name:"ashudhas",
-      img:"https://th.bing.com/th/id/OIP.8eJNMEsDP4uYuV4dl--i9gHaE8?w=356&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
-    },
-    
-    {
-      id:1,
-      name:"ashudhas",
-      img:"https://th.bing.com/th/id/OIP.8eJNMEsDP4uYuV4dl--i9gHaE8?w=356&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
-    },
-    
-    {
-      id:1,
-      name:"ashudhas",
-      img:"https://th.bing.com/th/id/OIP.8eJNMEsDP4uYuV4dl--i9gHaE8?w=356&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+      id:3,
+      name:" Sirisha Bandla",
+      img:image3,
+      description:"Sirisha Bandla (Telugu: శిరీష బండ్ల; c. 1988) is an Indian-American aeronautical engineer and space tourist.[2] She is the Vice President of Government Affairs and Research Operations for Virgin Galactic.[3] She flew on the Virgin Galactic Unity 22 mission which made her the second India-born woman to go to space and the fourth person of Indian descent ever to go past the line of space after Rakesh Sharma, Kalpana Chawla and Sunita Williams"
     }
+
+    // {
+    //   id:4,
+    //   name:"Reetha Mathew",
+    //   img:"https://th.bing.com/th/id/OIP.8eJNMEsDP4uYuV4dl--i9gHaE8?w=356&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+    // },
+
+    // {
+    //   id:5,
+    //   name:"Reetha Mathew",
+    //   img:"https://th.bing.com/th/id/OIP.8eJNMEsDP4uYuV4dl--i9gHaE8?w=356&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+    // },
+
+    // {
+    //   id:6,
+    //   name:"Reetha Mathew",
+    //   img:"https://th.bing.com/th/id/OIP.8eJNMEsDP4uYuV4dl--i9gHaE8?w=356&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+    // }
   ]
   return (
     <div className="App">
@@ -62,8 +71,9 @@ function App() {
           Learn React
         </a>
       </header>
-,<Button>hhello woprld</Button>
-<Project1 data={data}/>
+      <Button>click here</Button>
+      {/* <Project1 data={data}/> */}
+      <Proj2 data={data} />
     </div>
   );
 }
--- src/Component/Proj2.jsx
+++ src/Component/Proj2.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from "react"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import '../Component/Proj2.css'
 
-function Proj2() {
+function Proj2(props) {
+  const data=props.data
+  console.log(data);
   return (
-    <div>Proj2</div>
+    <>
+    <div className="c">
+    {
+    data.map((p1)=>(
+        <>
+            <Card className="a" style={{width:'30rem'}}>
+              <Card.Img variant="top" src={p1.img}/>
+              <Card.Body>
+                  <Card.Title>{p1.name}</Card.Title>
+                  <Card.Text>{p1.description} </Card.Text>
+                     <Button variant="primary">click here</Button>
+              </Card.Body>
+            </Card>
+            
+            </>
+    ))
+  }
+
+    </div>
+  
+
+        </>
   )
 }
 
